3da81c500068fa8c9a70f761a05bcd7e63292aef
lincoln <lwdthe1@gmail.com>
2016-12-05 00:14:20 -0500

    Adding navigation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import viewControllers.HomeFeedViewController;
+import viewControllers.MainApplication;
 
 /**
  * Created by lwdthe1 on 12/4/16.
@@ -7,10 +8,11 @@ public class Main {
     public static void main(String[] args) {
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
+
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new HomeFeedViewController();
+                new MainApplication();
             }
         });
     }
---
+++ src/main/java/viewControllers/AppViewController.java
@@ -0,0 +1,8 @@
+package viewControllers;
+
+/**
+ * Created by lwdthe1 on 12/5/16.
+ */
+public interface AppViewController {
+    void setupView();
+}
